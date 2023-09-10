6cc4f7b474bff953194f3427943a5ba1fac1f354
Ronalds-Z <ronaldsz123@inbox.lv>
2023-09-10 20:48:23 +0300

    comment fix/styling addon/nesting fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-import "/src/main.scss";
-import "/src/components/product-card.scss";
+import "/src/scss/main.scss";
 
-const productList = document.querySelector("procuct-card");
-if (productList.length < 6) {
-    for (let i = 0; i < productList.length; i++) {
-        console.log(`Processing product card ${i + 1}`);
-    }
-} else {
-    console.log("No product cards found");
-}
+import "/src/components/product-card.scss";
