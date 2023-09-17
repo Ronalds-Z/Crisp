b761c3df23f17aa998a1ee2bbb7f2e42a24cbf9c
Ronalds-Z <ronaldsz123@inbox.lv>
2023-09-17 18:55:13 +0300

    added swiper

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 import "/src/scss/main.scss";
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-import "/src/components/product-card.scss";
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: 5000,
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
