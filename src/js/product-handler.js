9fa52e09d6c27193056a3aee5251e1a6101ee4ed
Ronalds-Z <ronaldsz123@inbox.lv>
2023-10-30 00:30:26 +0200

    comments api// accordian menu// mobile view//burger menu

--- src/js/product-handler.js
+++ src/js/product-handler.js
@@ -7,7 +7,7 @@ export const appendProduct = (productElement, productCard, productImage, brand, 
     productCard.appendChild(clonedProductElement);
 }
 
-export const fetchProducts = async (productCard, productElement, counter, productUrl) => {
+export const fetchProducts = async (productCard, productElement, count, productUrl) => {
     const productHide = productCard.querySelector(".product-card");
     if (productHide) {
         productHide.remove();
@@ -19,13 +19,13 @@ export const fetchProducts = async (productCard, productElement, counter, produc
         .then(data => {
             if (productCard && productElement) {
                 data.forEach((element, index) => {
-                    const productImage = `https://picsum.photos/344/450/?v=${counter}`;
+                    const productImage = `https://picsum.photos/344/450/?v=${count}`;
                     appendProduct(productElement, productCard, productImage, element.brand, element.name, element.ibu)
-                    counter++;
+                    count++;
                 });
             }
         })
-    return counter;
+    return count;
 }
 
 const uniqueBrands = new Set();
@@ -45,12 +45,45 @@ export const fetchFilter = async (filterElement, filterWrap) => {
         .then(response => response.json())
         .then(data => {
             if (filterElement && filterWrap) {
-                    data.forEach(beer => {
-                        if (!uniqueBrands.has(beer.brand)) {
-                            appendFilter(filterElement, filterWrap, beer.brand);
-                            uniqueBrands.add(beer.brand);
-                        }
-                    });
+                data.forEach(beer => {
+                    if (!uniqueBrands.has(beer.brand)) {
+                        appendFilter(filterElement, filterWrap, beer.brand);
+                        uniqueBrands.add(beer.brand);
+                    }
+                });
             }
         });
+}
+
+
+export const appendArticle = (articleElement, articleWrap, user, body, email,) => {
+    const clonedArticleElement = articleElement.cloneNode(true);
+    clonedArticleElement.querySelector(".article-heading").innerText = user;
+    clonedArticleElement.querySelector(".article-text").innerText = body;
+    clonedArticleElement.querySelector(".article-credits").innerText = email;
+    articleWrap.appendChild(clonedArticleElement);
+};
+export const fetchArticle = async (articleElement, articleWrap) => {
+
+    const articleHide = articleWrap.querySelector(".article");
+    if (articleHide) {
+        articleHide.remove();
+    }
+
+    await fetch(`https://jsonplaceholder.typicode.com/comments`, {
+        method: "GET"
+    })
+        .then((response) => response.json())
+        .then(comments => {
+            if (articleElement && articleWrap) {
+                const randomComments = shuffleComments(comments);
+                randomComments.forEach((comment) => {
+                    appendArticle(articleElement, articleWrap, comment.name, comment.body, comment.email);
+                });
+            }
+        });
+}
+function shuffleComments(comments) {
+    const shuffled = comments.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 4);
 }
